f4e10a15cb71713bc1bc262ecb1d4796c9ee185e
KayongoYongo <sknsammie10@gmail.com>
2022-12-05 19:50:59 +0000

    update 0.1

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for i in matrix:
+            for j in i:
+                if j == i[-1]:
+                    print("{:d}".format(j), end="")
+                else:
+                    print("{:d}".format(j), end=" ")
+            print()
