9b63fd20a42d7068c73ee109b3fbca69ce4a3f39
KayongoYongo <sknsammie10@gmail.com>
2022-12-09 22:52:15 +0000

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+    v = list(a_dictionary.values())
+    k = list(a_dictionary.keys())
+    return k[v.index(max(v))]
