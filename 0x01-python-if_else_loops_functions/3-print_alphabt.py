2b30a5a4743361326260affdfa60d63a12bf5f2a
KayongoYongo <sknsammie10@gmail.com>
2022-12-28 12:17:27 +0000

    print alphabet except

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
-        print("{}".format(chr(letter)), end="")
+for i in range(97, 123):
+    if chr(i) == 'e' or chr(i) =='q':
+        continue
+    print(f"{chr(i)}", end="")
