4e4fed11864a3f0c6f119c820b424c6d9fb33b7e
KayongoYongo <sknsammie10@gmail.com>
2022-12-28 14:30:25 +0000

    Alphabet in reverse alternating

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""""Print the alphabet in reverse order alternating upper- and lower-case."""
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+
+for i in range(122, 96, -2):
+    print("{}".format(chr(i)), end = "")
+    print("{}".format(chr(i - 33)), end = "")
