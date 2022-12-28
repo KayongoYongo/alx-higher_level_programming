f04d4bf27b112a9b74db826c1db5e7990a4516d6
KayongoYongo <sknsammie10@gmail.com>
2022-12-28 12:24:30 +0000

    print lowercase except

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,6 +3,6 @@
 """Print the alphabet in lowercase, not followed by a new line."""
 
 for i in range(97, 123):
-    if chr(i) == 'e' or chr(i) =='q':
+    if chr(i) == 'e' or chr(i) == 'q':
         continue
     print("{}".format(chr(i)), end="")
