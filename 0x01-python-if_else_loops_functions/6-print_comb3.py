dbd75a63c256c03a2d66d7934e0b67d4e5433935
KayongoYongo <sknsammie10@gmail.com>
2022-12-28 13:21:18 +0000

    0 to 100 but lowest

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""Print all possible different combinations of two digits in ascending order.
-    The two digits must be different - 01 and 10 are considered identical.
-    """
-for i in range(0, 10):
-    for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print("{}{}".format(i, j))
-        else:
-            print("{}{}".format(i, j), end=", ")
+for i in range(0, 99):
+    if i % 10 < i / 10:
+        continue
+    if i == 89:
+        print("{:d}".format(i))
+    else:
+        print("{:02d}".format(i), end=", ")
