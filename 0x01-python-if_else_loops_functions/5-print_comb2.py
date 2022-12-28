d1136c8e496bb95c1f6e5e3fbe9a2e3e950b2c6c
KayongoYongo <sknsammie10@gmail.com>
2022-12-28 12:38:32 +0000

    print 2 digit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number == 99:
-        print("{}".format(number))
+        print("{:d}".format(number))
     else:
-        print("{:02}".format(number), end=", ")
+        print("{:02d}".format(number), end=", ")
