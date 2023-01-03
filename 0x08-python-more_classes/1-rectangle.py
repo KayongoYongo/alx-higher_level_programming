ea38e17225615932eeb92c4543a8c484b12297bd
KayongoYongo <sknsammie10@gmail.com>
2023-01-03 10:50:52 +0000

    Rectangle with properties

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-""" Creating a class claaed Rectangle"""
+""" Creating a class called Rectangle"""
 
 
 class Rectangle:
     """Instatiation of the class"""
 
     def __init__(self, width=0, height=0):
-        self.width = width
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.height = height
+        self.width = width
 
     """Using Property to get the value of width"""
 
     @property
     def width(self):
-        return (self.__width)
+        return self.__width
 
     """Using .setter to mutate the value of width"""
 
@@ -26,11 +31,13 @@ class Rectangle:
         self.__width = value
 
     """Using property to get the value of height"""
+
     @property
     def height(self):
         return (self.__height)
 
     """Using seter to mutate the value of height"""
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
