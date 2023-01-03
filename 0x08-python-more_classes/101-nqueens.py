85383194281eeee5e2addc154ebb6d40943ca8d0
KayongoYongo <sknsammie10@gmail.com>
2023-01-03 17:57:50 +0000

    chess board

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 
-"""Will try it out later"""
+import sys
+
+
+def printBoard(board):
+    if any(1 in x for x in board):
+        print([[idx, board[idx].index(1)] for idx, val in enumerate(board)])
