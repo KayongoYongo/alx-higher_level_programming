f7d5cd5200be8b35d602284acf6a238ab811e55d
KayongoYongo <sknsammie10@gmail.com>
2023-01-03 10:59:00 +0000

    create rectangle attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,43 +3,34 @@
 
 
 class Rectangle:
-    """Instatiation of the class"""
+    """make rectangle object"""
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
-        Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
-        """
         self.height = height
         self.width = width
 
-    """Using Property to get the value of width"""
-
     @property
     def width(self):
+        """gets width of the rectangle"""
         return self.__width
-
-    """Using .setter to mutate the value of width"""
 
     @width.setter
     def width(self, value):
+        """sets width of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    """Using property to get the value of height"""
-
     @property
     def height(self):
+        """gets height of the rectangle"""
         return (self.__height)
-
-    """Using seter to mutate the value of height"""
 
     @height.setter
     def height(self, value):
+        """sets height of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("height must be an iteger")
         elif value < 0:
