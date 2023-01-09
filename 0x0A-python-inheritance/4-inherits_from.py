5a8f68a5332d898d5f970e17cb45b7dee27fe629
KayongoYongo <sknsammie10@gmail.com>
2023-01-09 20:11:11 +0000

    Inherits

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""subclass checker"""
+
 
 def inherits_from(obj, a_class):
+    """returns subclass yes"""
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
