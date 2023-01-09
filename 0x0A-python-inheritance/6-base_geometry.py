2e0027142fd653c16760c142a3994ae57799b33b
KayongoYongo <sknsammie10@gmail.com>
2023-01-09 08:39:29 +0000

    Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+
+    def __init__(self):
+        """initialize class"""
+        pass
+
+    def area(self):
+        """Rasie Exception"""
+        raise Exception("area() is not implemented")
