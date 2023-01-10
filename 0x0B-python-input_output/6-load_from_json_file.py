ce42697e7cbbcf757555aae3bedad501ab714b5e
KayongoYongo <sknsammie10@gmail.com>
2023-01-10 21:17:21 +0000

    From JSON

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """does it"""
     with open(filename, mode='r', encoding='utf-8') as f:
-        return(json.load(f))
+        return (json.load(f))
