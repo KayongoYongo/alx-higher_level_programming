9198482e72ff5d9c39819ed34ffa7a91aaf7feb6
KayongoYongo <sknsammie10@gmail.com>
2023-01-17 13:48:21 +0000

    update 2.0

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,10 +5,12 @@ import json
 
 
 class Base:
+
     """defined a base class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """initialization"""
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
@@ -16,6 +18,7 @@ class Base:
             self.id = id
 
     def to_json_string(list_dictionaries):
+        """returns json object"""
         if len(list_dictionaries) == 0 or list_dictionaries is None:
             return "[]"
         else:
@@ -23,7 +26,20 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        my_dict = list_objs.to_dictionary()
-        my_json_string = to_json_string([my_dict])
+        """saves a json object to a file"""
+        result = []
+        filename = cls.__name__ + ".json"
+        if list_objs:
+            for i in list_objs:
+                result.append(cls.to_dictionary(i))
 
-        with open(self.__class__.__name__ as f:
+        with open(filename, 'w') as f:
+            f.write(cls.to_json_string(result))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns deserilizated json string"""
+        if json_string:
+            return json.loads(json_string)
+        else:
+           return []
