b941a7a4aaf97e76ae4f5ed83219b71aa0cc1c5d
KayongoYongo <sknsammie10@gmail.com>
2023-01-30 19:45:51 +0000

    so fucking proud for list comprehension

---
+++ 0x08-python-more_classes/tests/pattern.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+square = "".join([("\n".join(["".join(['#' for i in range (10)]) for j in range (5)]))])
+
+print(square)
+
+"""This script prints a pattern using list comprehension"""
+"""SO fucking proud""
+
