908615b1b4f1c26f350b4700c152f2c2d5620835
KayongoYongo <sknsammie10@gmail.com>
2023-02-12 15:44:31 +0000

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """Add all arguments to a Python list and save them to a file."""
 import sys
+import importlib
+
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    module = importlib.__import__('6-load_from_json_file')
+    load_from_json_file = module.load_from_json_file
 
     try:
         items = load_from_json_file("add_item.json")
