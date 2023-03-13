27cd53e717b94032f176b113d8d1cf467b249e22
KayongoYongo <sknsammie10@gmail.com>
2023-03-13 11:04:44 +0000

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
 const count = process.argv.length;
-if (count === 2) {
-  console.log('No argument');
-} else if (count === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Argument found');
-}
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
