aa97f22fa489b8198d84fa85bf6f234be962e61b
KayongoYongo <sknsammie10@gmail.com>
2023-03-13 14:04:26 +0000

    argv

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,2 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-console.log(args[0] === 'undefined' ? 'No argument' : process.argv[2]);
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
