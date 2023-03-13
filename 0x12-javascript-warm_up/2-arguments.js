f78e5e55589212d43804b9194babcc780a8facfd
KayongoYongo <sknsammie10@gmail.com>
2023-03-13 10:35:45 +0000

    Arguments found

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 const count = process.argv.length;
 if (count <= 2) {
   console.log('No argument');
+} else if (count ===3) {
+  console.log('Argument found');
 } else {
   console.log('Argument found');
 }
