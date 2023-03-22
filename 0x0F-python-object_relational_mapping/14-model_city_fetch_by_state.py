b8d2951d134c950b61667a7601fe14068cab26a7
KayongoYongo <sknsammie10@gmail.com>
2023-03-22 16:09:53 +0000

    task 14 update

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
