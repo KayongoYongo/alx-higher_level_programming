27996683b5ed32fc4738bf562ad3daeba887c2d0
KayongoYongo <sknsammie10@gmail.com>
2023-03-31 20:56:49 +0000

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A python script to open a URL"""
+
+
+import urllib.request
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
