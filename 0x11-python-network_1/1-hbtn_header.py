93a19783fdb3d630dc35db69a18605ecf285dcfb
KayongoYongo <sknsammie10@gmail.com>
2023-03-31 21:59:24 +0000

    hbtn header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+	url = sys.argv[1]
+
+	with urllib.request.urlopen(url) as response:
+    		x_request_id = response.getheader('X-Request-Id')
+
+	print(x_request_id)
