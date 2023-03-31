2213958e57602cb09faac1f2e7f90725337088b6
KayongoYongo <sknsammie10@gmail.com>
2023-03-31 22:11:17 +0000

    hbtn header update

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,9 +6,9 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
-	url = sys.argv[1]
+    url = sys.argv[1]
 
-	with urllib.request.urlopen(url) as response:
-    		x_request_id = response.getheader('X-Request-Id')
+    with urllib.request.urlopen(url) as response:
+        x_request_id = response.getheader('X-Request-Id')
 
-	print(x_request_id)
+    print(x_request_id)
