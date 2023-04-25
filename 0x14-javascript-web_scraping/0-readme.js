2c306c7cf686294a50b6f23fccb29d9da97d7fb4
KayongoYongo <sknsammie10@gmail.com>
2023-04-25 17:01:59 +0000

    C is fun

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -9,10 +9,9 @@ const filePath = process.argv[2];
 // Use the fs.readFile method to read the contents of the file
 // // with utf-8 encoding and a callback function to handle the result
 fs.readFile(filePath, 'utf8', (err, data) => {
-	if (err) {
-		console.error(err);
-	} else {
-
-		console.log(data);
-	}
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
