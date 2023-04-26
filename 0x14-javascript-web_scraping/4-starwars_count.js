fedad762d49d67f1aaa5250aaaf6be0ae3c8c316
KayongoYongo <sknsammie10@gmail.com>
2023-04-26 00:53:01 +0000

    character appearance

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,10 +6,10 @@ request(address, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    let results = JSON.parse(body).results;
+    const results = JSON.parse(body).results;
     let count = 0;
-    for (let i in results) {
-      for (let chr of results[i].characters) {
+    for (const i in results) {
+      for (const chr of results[i].characters) {
         if (chr.search('/18/') > 0) { count += 1; }
       }
     }
